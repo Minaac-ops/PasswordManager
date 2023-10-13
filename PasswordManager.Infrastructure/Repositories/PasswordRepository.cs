06dfd5b972dafe4a87b48b4c60c5f5f662db735d
Mina Anne Christensen <annechristensen88@live.dk>
2023-10-13 19:27:35 +0200

    ..

--- PasswordManager.Infrastructure/Repositories/PasswordRepository.cs
+++ PasswordManager.Infrastructure/Repositories/PasswordRepository.cs
@@ -1,10 +1,8 @@
-using System.Collections.Immutable;
 using MongoDB.Driver;
-using PasswordManager.Core.Interfaces;
 using PasswordManager.Core.Interfaces.Repositories;
-using PasswordManager.Core.Models;
 using PasswordManager.Infrastructure.Converters;
 using PasswordManager.Infrastructure.Schemas;
+using Microsoft.Extensions.Configuration;
 
 namespace PasswordManager.Infrastructure.Repositories;
 
@@ -12,14 +10,17 @@ public class PasswordRepository : IPasswordRepository
 {
     private readonly IMongoCollection<User> _users;
     private readonly UserConverter _converter;
+    private readonly IConfiguration _config;
     
     public PasswordRepository()
     {
+        _config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+        
         _converter = new UserConverter();
-        const string connectionuri =
-            "mongodb+srv://annechristensen:BVu4uofJTGhw1J9u@cluster0.eljsoo0.mongodb.net/?retryWrites=true&w=majority";
 
-        var settings = MongoClientSettings.FromConnectionString(connectionuri);
+        var settings = MongoClientSettings.FromConnectionString(_config.GetConnectionString("MongoDB"));
         var client = new MongoClient(settings);
         var db = client.GetDatabase("PasswordManager");
 
